54f034b6dcce9a4bef78123f943abc16485d418e
vaarun2104 <varunrajbathini@gmail.com>
2026-02-27 17:34:07 +0530

    Logs creation is completed and tested

--- Test_Cases/Test_Admin_Login.py
+++ Test_Cases/Test_Admin_Login.py
@@ -4,6 +4,8 @@ from selenium.webdriver.common.by import By
 
 from base_pages.Login_Admin_Page import Login_Admin_Page
 from Utilities.read_properties import Read_Config
+from Utilities.custom_logger import Log_Maker
+
 
 class Test_01_Admin_Login:
 
@@ -11,22 +13,26 @@ class Test_01_Admin_Login:
     username = Read_Config.get_username()
     password = Read_Config.get_password()
     invalid_username = Read_Config.get_invalid_username()
+    logger = Log_Maker.log_gen()
 
     def test_title_verification(self,setup):
+        self.logger.info("**********Test_01_Admin_Login************")
+        self.logger.info("**********verification of admin login page title***********")
         self.driver = setup
         self.driver.get(self.admin_page_url)
-
         act_title = self.driver.title
         exp_title = "nopCommerce demo store. Login"
-
         if act_title == exp_title:
+            self.logger.info("**********test_title_verification title matched************")
             assert True
             self.driver.close()
         else:
             self.driver.save_screenshot(".\\screenshots\\test_title_verification.png")
+            self.logger.info("**********test_title_verification title not matched************")
             self.driver.close()
             assert False
     def test_valid_admin_login(self,setup):
+        self.logger.info("**********test_valid_admin_login started************")
         self.driver = setup
         self.driver.get(self.admin_page_url)
         self.admin_lp = Login_Admin_Page(self.driver)
@@ -35,6 +41,7 @@ class Test_01_Admin_Login:
         self.admin_lp.click_log()
         act_dashboard_text = self.driver.find_element(By.XPATH, "//div[@class='content-header']//h1").text
         if act_dashboard_text == "Dashboard":
+            self.logger.info("**********Dashboard text found ************")
             assert True
             self.driver.close()
         else:
@@ -43,6 +50,7 @@ class Test_01_Admin_Login:
             assert False
 
     def test_invalid_admin_login(self,setup):
+        self.logger.info("**********test_invalid_admin_login started ************")
         self.driver = setup
         self.driver.get(self.admin_page_url)
         self.admin_lp = Login_Admin_Page(self.driver)
@@ -51,6 +59,7 @@ class Test_01_Admin_Login:
         self.admin_lp.click_log()
         error_message = self.driver.find_element(By.XPATH,"//li").text
         if error_message == "No customer account found":
+            self.logger.info("**********test_invalid_admin_login Error Messages matched ************")
             assert True
             self.driver.close()
         else:
