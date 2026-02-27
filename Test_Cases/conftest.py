7bfdc6f63a0301fb97ddd75f37361200d20c3fa3
vaarun2104 <varunrajbathini@gmail.com>
2026-02-27 18:31:43 +0530

    Running Tests On specific Browsers

--- Test_Cases/conftest.py
+++ Test_Cases/conftest.py
@@ -1,7 +1,25 @@
 import pytest
 from selenium import webdriver
 
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="chrome",
+                     help="Specify the browser: chrome or firefox or edge")
+
 @pytest.fixture()
-def setup():
-    driver = webdriver.Chrome()
+def browser(request):
+    return request.config.getoption("--browser")
+
+
+@pytest.fixture()
+def setup(browser):
+    global driver
+    if browser == "chrome":
+        driver = webdriver.Chrome()
+    elif browser == "firefox":
+        driver = webdriver.Firefox()
+    elif browser == "edge":
+        driver = webdriver.Edge()
+    else:
+        raise ValueError("Unsupported browser")
     return driver
