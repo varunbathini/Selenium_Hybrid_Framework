dc76de79c8dc34ac08c78dd6d791c3168a2df2df
vaarun2104 <varunrajbathini@gmail.com>
2026-03-07 21:53:26 +0530

    Generating Html Reports

--- Test_Cases/conftest.py
+++ Test_Cases/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 from selenium import webdriver
+from pytest_metadata.plugin import metadata_key
 
 
 def pytest_addoption(parser):
@@ -22,3 +23,17 @@ def setup(browser):
     else:
         raise ValueError("Unsupported browser")
     return driver
+
+
+########## for pytest html reports ##########
+# hook for adding environment info in html report
+def pytest_configure(config):
+    config.stash[metadata_key]['Project Name'] = 'Ecommerce Project, nopcommerce'
+    config.stash[metadata_key]['Test Module Name'] = 'Admin Login Tests'
+    config.stash[metadata_key]['Tester Name'] = 'Varun'
+# hook for delete/modify environment info in html report
+@pytest.mark.optionalhook
+def pytest_metadata(metadata):
+    metadata.pop('JAVA_HOME', None)
+    metadata.pop('Plugins', None)
+
